e91c2f1671158540db70cfa6d3025b843a3b30b7
bi0punk <systecinfo_cl@criptext.com>
2023-11-27 01:55:24 -0300

    add console

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-import requests
+import socket
+import time
 
-while True:
-    command = input("Ingrese el comando (e.g., open1, close2) o 'q' para salir: ")
-    if command.lower() == 'q':
-        break
-    
-    url = f"http://192.168.1.115/command?command={command}"
-    response = requests.get(url)
-    
-    print(f"Respuesta del servidor: {response.text}")
+# Dirección IP y puerto del Arduino
+arduino_ip = "192.168.1.177"
+arduino_port = 80
+
+# Función para enviar comandos al Arduino
+def send_command(command):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((arduino_ip, arduino_port))
+        s.sendall(command.encode())
+        data = s.recv(1024)
+    print("Respuesta del Arduino:", data.decode())
+
+# Enviar comandos al Arduino (puedes ajustar estos comandos según tus necesidades)
+send_command("on1")
+time.sleep(2)  # Esperar 2 segundos
+send_command("off1")
